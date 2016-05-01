1b3a91e7c94c0331eed600dcf01f2574ed19e0e9
StrixG <obrekht@gmail.com>
2016-05-01 14:27:57 +0200

    Setup ButterKnife

--- app/src/main/java/com/obrekht/todoapp/MainActivity.java
+++ app/src/main/java/com/obrekht/todoapp/MainActivity.java
@@ -3,16 +3,22 @@ package com.obrekht.todoapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        Log.d(TAG, "onCreate: " );
+        setSupportActionBar(toolbar);
     }
 }
