84946fdecfc0318274964eb5a1074a7384864d61
StrixG <obrekht@gmail.com>
2016-05-07 00:53:53 +0200

    Add Project

---
+++ app/src/main/java/com/beefficient/Project.java
@@ -0,0 +1,18 @@
+package com.beefficient;
+
+// TODO: add color
+public class Project {
+    private String name;
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
