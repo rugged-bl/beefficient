56d02452590c8af2de99456d8c54a8b1ec3464d4
StrixG <obrekht@gmail.com>
2016-05-06 17:10:38 +0200

    Update

--- app/src/main/java/com/beefficient/MainActivity.java
+++ app/src/main/java/com/beefficient/MainActivity.java
@@ -3,13 +3,19 @@ package com.beefficient;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
